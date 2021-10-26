6013598629c150fb39d5194cd3385239ac04283a
Your Name <you@example.com>
2021-10-26 11:55:40 +0700

    first commit

---
+++ src/application/TestController.java
@@ -0,0 +1,67 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import org.json.JSONException;
+
+import Controller.GDTT_Hose_Controller;
+import DAO.GDTT_Hose_DAO;
+import DoAn.GDTT_Hose;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class TestController {
+	@FXML 
+	private TableView<GDTT_Hose> table;
+	@FXML
+    private Button Btn_Show;
+	@FXML
+    private Button Btn_Refill;
+	@FXML
+    private TableColumn<GDTT_Hose, String> colId;
+    @FXML
+    private TableColumn<GDTT_Hose, Double> colPrice;
+    @FXML
+    private TableColumn<GDTT_Hose, Integer> colAmount;
+    @FXML
+    private TableColumn<GDTT_Hose, Double> colValue;
+    @FXML
+    private TableColumn<GDTT_Hose, String> colTime;
+    
+    ObservableList<GDTT_Hose> listM = FXCollections.observableArrayList();
+    
+    public void initialize(URL location, ResourceBundle resources) {
+            
+    }
+    public void show(ActionEvent e) {
+    	
+    	
+    	colId.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getId()));
+    	colPrice.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getPrice()).asObject());
+    	colAmount.setCellValueFactory(cellData ->new SimpleIntegerProperty(cellData.getValue().getAmount()).asObject());
+    	colValue.setCellValueFactory(cellData ->new SimpleDoubleProperty(cellData.getValue().getValue()).asObject());
+    	colTime.setCellValueFactory(cellData ->new SimpleStringProperty(cellData.getValue().getTime()));
+    	
+    	listM = GDTT_Hose_DAO.findAll();
+    	
+    	table.setItems(listM);
+    }
+    public void refill(ActionEvent e) throws JSONException, IOException {
+    	GDTT_Hose_Controller.handle();
+    }
+}
